69bb7549394515fdcac4262c5ad9f857fc3dae00
gasparpeter <petihappygaspar@gmail.com>
2019-08-30 05:29:35 -0700

    - created btn, onClick increaseing the p value

--- src/Awesome.js
+++ src/Awesome.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default (props) => {
+
+    const [ counter, setCounter ] = useState(0);
+
+    const inCreaseCounter = () => {
+        setCounter(counter +1)
+    };
+
     return(
-        <div>Awesome</div>
+        <div>
+            <p>Counter: {counter}</p>
+            <button onClick={inCreaseCounter} type='button'>
+                Add + 1
+            </button>
+        </div>
     )
 }
